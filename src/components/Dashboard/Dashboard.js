ccd51df4eb646c78dd3a87982ca731a9f4681d8e
Mary Gruen <marygruen@Marys-Air.lan>
2020-07-09 21:37:25 -0700

    add items to nav

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,13 +1,18 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import Row from "@sendgrid/ui-components/grid/row";
 import DashboardNavigation from "../DashboardNavigation";
 import Navigation from "../Navigation";
 
-export default class Dashboard extends PureComponent {
+export default class Dashboard extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", email: "" };
+  }
+
   render() {
     return (
       <div className="Dashboard">
-        <Navigation />
+        <Navigation name={this.state.name} />
         <Row>
           <DashboardNavigation />
         </Row>
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,75 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import "./Navigation.css";
+import classnames from "classnames";
 
-export default class Navigation extends PureComponent {
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isProfileDropdownOpen: false };
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  dropdownClasses() {
+    return classnames({
+      btn: true,
+      "btn-tertiary": true,
+      "btn-dropdown": true,
+      "has-icon": true,
+      dropdown: true,
+      "is-active": this.state.isProfileDropdownOpen,
+    });
+  }
+
+  toggleDropdown() {
+    this.setState((prevState) => ({
+      isProfileDropdownOpen: !prevState.isProfileDropdownOpen,
+    }));
+  }
+
   render() {
     return (
       <div className="Navigation">
         <div className="Navigation-logo" alt="logo" />
-        <div class="Navigation-buttonList btn-list">
-          <button class="btn btn-primary">Sign up</button>
-          <button class="btn btn-secondary">Log in</button>
+        <div className="Navigation-items">
+          <span className="Navigation-item">
+            Products
+            <i className="sg-icon sg-icon-caret" />
+          </span>
+          <span className="Navigation-item">
+            Docs
+            <i className="sg-icon sg-icon-caret" />
+          </span>
+          <span className="Navigation-item">
+            About
+            <i className="sg-icon" />
+          </span>
+          <span className="Navigation-item">
+            Careers
+            <i className="sg-icon" />
+          </span>
         </div>
+        {this.props.name ? (
+          <div className={this.dropdownClasses()} onClick={this.toggleDropdown}>
+            <i className="sg-icon sg-icon-teammate" />
+            {this.props.name}
+            <ul className="dropdown-menu">
+              <li>
+                <a class="dropdown-link">Dashboard</a>
+              </li>
+              <li>
+                <a class="dropdown-link">Settings</a>
+              </li>
+              <li>
+                <a class="dropdown-link">Log out</a>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="Navigation-buttonList btn-list">
+            <button className="btn btn-secondary">Log in</button>
+            <button className="btn btn-primary">Sign up</button>
+          </div>
+        )}
       </div>
     );
   }
